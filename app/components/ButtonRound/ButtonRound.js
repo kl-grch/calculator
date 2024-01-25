7cfcdb94eb9d93fc2ebe8894eae757969adf7a65
Nikolay Gerasimov <kolyagerich@gmail.com>
2024-01-25 18:15:53 +0300

    fixed

--- app/components/ButtonRound/ButtonRound.js
+++ app/components/ButtonRound/ButtonRound.js
@@ -4,25 +4,22 @@ import { useContext } from "react";
 import "./buttonRound.scss";
 import { TypeContext } from "@/app/TypeContext";
 
-export default function ButtonRound({ value, onClick, style }) {
+export default function ButtonRound({ value, onClick }) {
   const type = useContext(TypeContext);
   let className;
-  let zeroStyle;
 
   if (type === "sign") {
     className = " button--sign";
   } else if (type === "other") {
     className = " button--other";
+  } else if (type === "number" && value === 0) {
+    className = " zero";
   } else {
     className = "";
   }
-  
+
   return (
-    <button
-      className={"button" + className}
-      onClick={onClick}
-      style={style}
-    >
+    <button className={"button" + className} onClick={onClick}>
       {value}
     </button>
   );
--- app/page.js
+++ app/page.js
@@ -1,136 +1,202 @@
 "use client";
+
 import "./homePage.scss";
 
-import ButtonRound from "./components/ButtonRound/ButtonRound";
 import { useState } from "react";
 import { TypeContext } from "./TypeContext";
+import ButtonRound from "./components/ButtonRound/ButtonRound";
 
 const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-const signs = ["÷", "×", "−", "+"];
+const signs = ["÷", "×", "-", "+"];
+const functionalButtons = ["AC", "±", "%"];
 
 export default function Home() {
-  const [numberOne, setNumberOne] = useState(0); // первое число
-  const [enterNumberOne, setEnterNumberOne] = useState(false); // статус нажатия на первое число
+  const [numberOne, setNumberOne] = useState(0);
+  const [enterNumberOne, setEnterNumberOne] = useState(true);
+  const [numberTwo, setNumberTwo] = useState(0);
+  const [enterNumberTwo, setEnterNumberTwo] = useState(false);
+  const [sign, setSign] = useState(null);
+  const [enterSign, setEnterSign] = useState(false);
+  const [result, setResult] = useState(false);
+  const [enterSignDot, setEnterSignDot] = useState(false);
 
-  const [numberTwo, setNumberTwo] = useState(0); // второе число
-  const [enterNumberTwo, setEnterNumberTwo] = useState(false); // статус нажатия на второе число
-
-  const [result, setResult] = useState(0); // результат
-  const [enterResult, setEnterResult] = useState(false); // статус вычисления результата
-
-  const [sign, setSign] = useState(null); // знак вычисления между первым и вторым числом
-  const [enterSign, setEnterSign] = useState(false); // статус нажатия знака
-
-  function handleClickNumberOne(number) {
-    setEnterNumberOne(true);
-    setNumberOne((nO) => Number(nO));
-    setNumberOne((nO) => String(nO) + number);
-    setNumberOne((nO) => Number(nO));
+  // ✅ Кнопки ввода чисел
+  // Доработать ввод чисел, при нажатие на 0 с установленным значение запятой, не вводится число 0
+  function handleClickNumber(number) {
+    if (enterNumberOne && !enterNumberTwo && !enterSign && !result) {
+      getNumberOne(number);
+    } else if (enterNumberOne && !enterNumberTwo && enterSign) {
+      setEnterNumberTwo(true);
+      setEnterNumberOne(false);
+      getNumberTwo(number);
+      setResult(false);
+    } else if (!enterNumberOne && enterNumberTwo && enterSign && !result) {
+      getNumberTwo(number);
+    }
   }
+
+  // ✅ Функция ввода первого числа
+  function getNumberOne(number) {
+    if (Number.isInteger(number) && enterSignDot) {
+      setNumberOne((nO) => Number(nO)); // 1
+      setNumberOne((nO) => String(`${nO}.`) + number); //
+      setNumberOne((nO) => Number(nO));
+      setEnterSignDot(false);
+    } else if (!Number.isInteger(number) && !enterSignDot) {
+      setNumberOne((nO) => Number(nO));
+      setNumberOne((nO) => String(nO) + number);
+      setNumberOne((nO) => Number(nO));
+    } else {
+      setNumberOne((nO) => Number(nO));
+      setNumberOne((nO) => String(nO) + number);
+      setNumberOne((nO) => Number(nO));
+    }
+  }
+
+  // ✅ Функция ввода второго числа
+  function getNumberTwo(number) {
+    if (Number.isInteger(number) && enterSignDot) {
+      setNumberTwo((nT) => Number(nT));
+      setNumberTwo((nT) => String(`${nT}.`) + number);
+      setNumberTwo((nT) => Number(nT));
+      setEnterSignDot(false);
+    } else if (!Number.isInteger(number) && !enterSignDot) {
+      setNumberTwo((nT) => Number(nT));
+      setNumberTwo((nT) => String(nT) + number);
+      setNumberTwo((nT) => Number(nT));
+    } else {
+      setNumberTwo((nT) => Number(nT));
+      setNumberTwo((nT) => String(nT) + number);
+      setNumberTwo((nT) => Number(nT));
+    }
+  }
+
+  // ✅ Нажатие на кнопку равно
+  function handleClickSignEqual() {
+    function setStateEqual() {
+      setResult(true);
+      setEnterSign(true);
+      setEnterNumberOne(true);
+      setNumberTwo(numberTwo);
+      setEnterNumberTwo(false);
+    }
+    if (sign === null) {
+      false;
+    } else if (sign === "+") {
+      setNumberOne(numberOne + numberTwo);
+      setStateEqual();
+    } else if (sign === "-") {
+      setNumberOne(numberOne - numberTwo);
+      setStateEqual();
+    } else if (sign === "×") {
+      setNumberOne(numberOne * numberTwo);
+      setStateEqual();
+    } else if (sign === "÷") {
+      setNumberOne(numberOne / numberTwo);
+      setStateEqual();
+    }
+  }
+
+  //  ✅ Операционные кнопки делить / умножить / вычесть / сложить / равно
+  // Доработать повторное нажатие на кнопку, при повторном нажатии на кнопку сначала должно вычислиться выражение нажатое на первую кнопку знака
 
   function handleClickSign(sign) {
-    setEnterSign(true);
-    setSign(sign);
-  }
-
-  function handleClickNumberTwo(number) {
-    setEnterNumberOne(false);
-    setEnterNumberTwo(true);
-    setNumberTwo((nT) => Number(nT));
-    setNumberTwo((nT) => String(nT) + number);
-    setNumberTwo((nT) => Number(nT));
-  }
-
-  function handleClickResult(numberOne, numberTwo, sign) {
-    let result;
-    switch (sign) {
-      case "+":
-        result = numberOne + numberTwo;
-        break;
-      case "−":
-        result = numberOne - numberTwo;
-        break;
-      case "×":
-        result = numberOne * numberTwo;
-        break;
-      case "÷":
-        result = numberOne / numberTwo;
-        break;
-      default:
-        null;
-    }
-    setResult(result);
-    setNumberOne(result);
-    setEnterNumberOne(true);
-    setNumberTwo(0);
-    setEnterNumberTwo(false);
-    setEnterSign(false);
-  }
-
-  function handleClickSignDot() {
-    // setNumberOne((nO) => Number(nO));
-    // setNumberOne((nO) => String(nO) + number);
-    // setNumberOne((nO) => Number(nO));
-  }
-
-  function getResult() {
-    if (enterResult) {
-      return result;
-    } else if (enterNumberOne) {
-      return numberOne;
-    } else if (enterNumberTwo) {
-      return numberTwo;
+    if (!enterNumberOne && enterNumberTwo && enterSign && !result) {
+      let calculation;
+      if (sign === "+") {
+        calculation = numberOne + numberTwo;
+      } else if (sign === "-") {
+        calculation = numberOne - numberTwo;
+      } else if (sign === "÷") {
+        if (numberTwo === 0) {
+          calculation = 0;
+        } else {
+          calculation = numberOne / numberTwo;
+        }
+      } else if (sign === "×") {
+        calculation = numberOne * numberTwo;
+      }
+      setResult(true);
+      setEnterSign(true);
+      setNumberOne(calculation);
+      setEnterNumberOne(true);
+      setNumberTwo(0);
+      setEnterNumberTwo(false);
     } else {
+      setSign(sign); // нажимаем на кнопку и устанавливаем значение знака
+      setEnterSign(true); // устанавливаем значение, что кнопка знака нажата
+    }
+  }
+
+  // ✅ Функция вывода числа на экран
+  function getResult() {
+    if (enterNumberOne) {
       return numberOne;
+    } else {
+      return numberTwo;
     }
   }
 
-  function handleClickClearData() {
-    setNumberOne(0);
-    setEnterNumberOne(false);
-    setNumberTwo(0);
-    setEnterNumberTwo(false);
-    setResult(0);
-    setEnterResult(false);
-    setSign(null);
-    setEnterSign(false);
-  }
-
-  console.log({
-    numberOne,
-    sign,
-    numberTwo,
-    enterSign,
-    result,
-    enterNumberOne,
-    enterNumberTwo,
-  });
-
-  function handleClickReverseData() {
-    if (enterNumberOne) {
-      return setNumberOne(-numberOne);
-    } else if (enterNumberTwo) {
-      return setNumberTwo(-numberTwo);
+  // ✅ Функциональные кнопки очистить данные / плюс - минус / процент от числа
+  function handleClickFunctionalButton(functionalButton) {
+    if (functionalButton === "AC") {
+      if (
+        numberOne === 0 &&
+        enterNumberOne &&
+        numberTwo === 0 &&
+        !enterNumberTwo &&
+        sign === null &&
+        !enterSign &&
+        !result &&
+        !enterSignDot
+      ) {
+        false;
+      } else {
+        setNumberOne(0);
+        setEnterNumberOne(true);
+        setNumberTwo(0);
+        setEnterNumberTwo(false);
+        setSign(null);
+        setEnterSign(false);
+        setResult(false);
+        setEnterSignDot(false);
+      }
+    } else if (functionalButton === "±") {
+      if (enterNumberOne && numberOne !== 0) {
+        setNumberOne(-numberOne);
+      } else if (enterNumberTwo && numberTwo !== 0) {
+        setNumberTwo(-numberTwo);
+      }
+    } else if (functionalButton === "%") {
+      if (enterNumberOne && numberOne !== 0) {
+        setNumberOne(numberOne / 100);
+      } else if (enterNumberTwo && numberTwo !== 0) {
+        setNumberTwo(numberTwo / 100);
+      }
     }
   }
 
-  function handleClickProcentage() {
-    if (enterNumberOne) {
-      return setNumberOne(numberOne / 100);
-    } else if (enterNumberTwo) {
-      return setNumberTwo(numberTwo / 100);
+  // ✅ Кнопка установки значения запятой
+  function handleClickSignDot() {
+    if (Number.isInteger(numberOne) && enterNumberOne) {
+      setEnterSignDot(true);
+    } else if (Number.isInteger(numberTwo) && enterNumberTwo) {
+      setEnterSignDot(true);
     }
   }
 
-  function handleClickNumber(number) {
-    // !enterSign
-    //   ? () => handleClickNumberOne(number)
-    //   : () => handleClickNumberTwo(number);
-    if (!enterSign) {
-      return handleClickNumberOne(number);
-    }
-    return handleClickNumberTwo(number);
-  }
+  console.log(
+    `
+    numberOne: ${numberOne}
+    numberTwo: ${numberTwo}
+    sign: ${sign}
+    enterNumberOne: ${enterNumberOne}
+    enterNumberTwo: ${enterNumberTwo}
+    enterSign: ${enterSign}
+    result: ${result}
+    enterSignDot: ${enterSignDot}
+    `
+  );
 
   return (
     <main>
@@ -141,16 +207,13 @@ export default function Home() {
         <div className="calculator__buttons">
           <TypeContext.Provider value={"other"}>
             <div className="buttons__memory">
-              <ButtonRound
-                value={
-                  enterNumberOne || enterNumberTwo || enterResult || enterSign
-                    ? "C"
-                    : "AC"
-                }
-                onClick={handleClickClearData}
-              />
-              <ButtonRound value="±" onClick={handleClickReverseData} />
-              <ButtonRound value="%" onClick={handleClickProcentage} />
+              {functionalButtons.map((functionalButton, i) => (
+                <ButtonRound
+                  key={i}
+                  value={functionalButton}
+                  onClick={() => handleClickFunctionalButton(functionalButton)}
+                />
+              ))}
             </div>
           </TypeContext.Provider>
 
@@ -163,28 +226,22 @@ export default function Home() {
                   onClick={() => handleClickSign(sign)}
                 />
               ))}
-              <ButtonRound
-                value="="
-                onClick={() => handleClickResult(numberOne, numberTwo, sign)}
-              />
+              <ButtonRound value="=" onClick={handleClickSignEqual} />
             </div>
           </TypeContext.Provider>
 
-          <div className="buttons__numbers">
-            {numbers.map((number) => (
-              <ButtonRound
-                key={number}
-                value={number}
-                style={
-                  number == 0
-                    ? { gridArea: "zero", width: "100%", borderRadius: "80px" }
-                    : null
-                }
-                onClick={() => handleClickNumber(number)}
-              />
-            ))}
-            <ButtonRound value="," onClick={handleClickSignDot} />
-          </div>
+          <TypeContext.Provider value={"number"}>
+            <div className="buttons__numbers">
+              {numbers.map((number) => (
+                <ButtonRound
+                  key={number}
+                  value={number}
+                  onClick={() => handleClickNumber(number)}
+                />
+              ))}
+              <ButtonRound value="," onClick={handleClickSignDot} />
+            </div>
+          </TypeContext.Provider>
         </div>
       </div>
     </main>
